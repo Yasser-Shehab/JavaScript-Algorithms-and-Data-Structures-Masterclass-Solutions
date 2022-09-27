ff6e0dbf08d7055dc04e37bdf1de661aac341bae
AquaApple <yassershehab9@gmail.com>
2022-09-27 19:05:20 +0200

    Added new solution

--- Problem Solving Patterns/Multiple Pointers/CountUniqueValues.js
+++ Problem Solving Patterns/Multiple Pointers/CountUniqueValues.js
@@ -1,4 +1,8 @@
+//Solution Using While loop
 function countUniqueValues(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
   let first = 0;
   let second = 1;
 
@@ -11,11 +15,21 @@ function countUniqueValues(arr) {
     }
   }
 
-  if (arr.length === 0) {
-    return first;
-  }
-
   return first + 1;
+}
+
+function countUniqueValues2(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  let i = 0;
+  for (let j = 1; j < arr.length; i++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
 }
 
 console.log(countUniqueValues([]));
